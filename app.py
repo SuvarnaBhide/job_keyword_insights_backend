801e6ca455a7352ce5f29d131526e290b0e9fe77
SuvarnaBhide <suvarna.p.bhide@gmail.com>
2024-07-02 15:30:27 +0530

    retrieve all data api call added

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, abort, request
 from flask_cors import CORS
+from datetime import datetime, timezone
 import csv
 import os
 import db
@@ -42,14 +43,22 @@ def flask_mongodb_atlas():
 
 @app.route("/test")
 def test():
-    db.db.collection.insert_one({"name": "John"})
+    db.db.test.insert_one({"name": "suzie"})
     return "Connected to the data base!"
 
 @app.route('/api/add_data', methods=['POST'])
 def add_data():
     data = request.json
+    data['created_at'] = datetime.now(timezone.utc)
     result = db.db.collection.insert_one(data)
     return jsonify({'inserted_id': str(result.inserted_id)}), 201
+
+@app.route('/api/read_all_data', methods=['GET'])
+def read_all_data():
+    # Sort by 'created_at' field in descending order
+    names = db.db.collection.find({}, {'_id': 0, 'name': 1}).sort('created_at', -1)
+    names_list = [item['name'] for item in names]
+    return jsonify(names_list)
 
 @app.route('/api/keyword_counts', methods=['GET'])
 def get_keyword_counts():
