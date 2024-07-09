e3d4e5973b652d31d5af309102f088785757f3c4
SuvarnaBhide <suvarna.p.bhide@gmail.com>
2024-07-09 16:31:06 +0530

    free mysql hosting done

--- config.py
+++ config.py
@@ -1,5 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from the .env file
+load_dotenv()
+
+# Get the MongoDB URI from environment variables
+mysql_uri = os.getenv('MYSQL_URI')
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:cycle123@localhost:3306/QUIZ_SCHEMA'
+    SQLALCHEMY_DATABASE_URI = mysql_uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
