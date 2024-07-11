0d033deec856abba5faf7276101325d22f084743
SuvarnaBhide <suvarna.p.bhide@gmail.com>
2024-07-11 17:14:20 +0530

    logic for inserting queries modified

--- csv_to_sql.py
+++ csv_to_sql.py
@@ -1,11 +1,13 @@
 import os
 import csv
+import random
 from sqlalchemy import create_engine, func
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 from config import Config
 from models import db, Quiz, Question, MCQOption
 
+# Load environment variables from the .env file
 load_dotenv()
 
 database_uri = Config.SQLALCHEMY_DATABASE_URI
@@ -16,9 +18,7 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 # Define the CSV file path
-csv_file_path = 'quiz_csvs/Computer Architecture.csv'
-# csv_file_path = 'quiz_csvs/React.csv'
-
+csv_file_path = 'quiz_csvs/Thermodynamics.csv'
 
 # Extract quiz name from the CSV file name
 quiz_name = os.path.splitext(os.path.basename(csv_file_path))[0]
@@ -41,17 +41,31 @@ with open(csv_file_path, 'r', newline='') as file:
     for row in reader:
         question_content = row['Question']
         correct_answer = row['CorrectAnswer']
-        options = [row['Option1'], row['Option2'], row['Option3'], row.get('Option4')]
+        options = [row['Option1'], row['Option2'], row['Option 3']]
+        
+        # Define the option labels
+        option_labels = ['A', 'B', 'C', 'D']
 
+        # Shuffle the option labels
+        shuffled_labels = option_labels[:]
+        random.shuffle(shuffled_labels)
+
+        # Assign the correct answer to a random label
+        correct_label = shuffled_labels.pop()
+        options_dict = dict(zip(shuffled_labels, options))
+        options_dict[correct_label] = correct_answer
+
+        # Insert the question
         new_question = Question(quiz_id=quiz_id, content=question_content)
         session.add(new_question)
         session.commit()
 
         question_id = new_question.id
-        option_labels = ['A', 'B', 'C', 'D']
-        for label, option_content in zip(option_labels, options):
+
+        # Insert the options
+        for label, option_content in zip(option_labels, options_dict.values()):
             if option_content:  # Only add options if the content is not empty
-                is_correct = option_content == correct_answer
+                is_correct = (option_content == correct_answer)
                 new_option = MCQOption(
                     question_id=question_id,
                     option_label=label,
